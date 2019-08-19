d8fb54427e128c7a4d8a52b27664f568df4273bb
Adam Bertram <adbertram@gmail.com>
2019-08-19 10:08:31 -0500

    apppveyor updates

--- buildscripts/install.ps1
+++ buildscripts/install.ps1
@@ -14,7 +14,7 @@ try {
     $requiredModules = @('PSPostMan')
     foreach ($m in $requiredModules) {
         Write-Host "Installing [$($m)] module..."
-        Install-Module -Name $m -Force -Confirm:$false
+        Install-Module -Name $m -Force -Confirm:$false -AllowClobber
         Write-Host "Removing [$($m)] module from current session..."
         Remove-Module -Name $m -Force -ErrorAction Ignore
         Write-Host "Importing [$($m)] module into current session..."
