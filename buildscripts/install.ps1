9582d27ba87c4be2a8aa9bf14573dcdaaae4caf6
Adam Bertram <adbertram@gmail.com>
2019-08-19 10:14:53 -0500

    appveyor updates

--- buildscripts/install.ps1
+++ buildscripts/install.ps1
@@ -11,7 +11,7 @@ try {
     # $null = Install-PackageProvider @provParams
     # $null = Import-PackageProvider @provParams
 
-    $requiredModules = @('PSPostMan')
+    $requiredModules = @('PowerShellGet')
     foreach ($m in $requiredModules) {
         Write-Host "Installing [$($m)] module..."
         Install-Module -Name $m -Force -Confirm:$false -AllowClobber
